a848109245d08da443ca5c1c6de8ee78792cd0f4
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-03-11 11:43:10 +1100

    Refactor homepage layout and styles; update content for Sonna Studios

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,73 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <main className={styles.container}>
+        <section className="home-introduction-section">
+          <div className="home-introduction-section-content">
+            <h1>Sonna Studios</h1>
+            <h2>Photography & Videography</h2>
+            <p>Make an Enquiry</p>
+            <p>
+              Sonna is a premium event photo & video coverage for Australia &
+              New Zealand, capturing the biggest and best events in town.
+              Working alongside global and local brands alike, Sonna is the
+              ‘go-to’ event photographer and videographer in NZ and a preferred
+              supplier to government departments, charities, businesses, brands
+              and corporations.
+            </p>
+          </div>
+        </section>
+        <div className="home-image-text-module">
+          <p>
+            We deliver premium event photo & video coverage New Zealand wide,
+            capturing the biggest and best events in town. Working alongside
+            global and local brands alike, Sonna is the ‘go-to’ event
+            photographer and videographer in NZ and a preferred supplier to
+            government departments, charities, businesses, brands and
+            corporations.
+          </p>
+        </div>
+        <div className="home-medium-paragraph-module">
+          <h2>
+            We deliver premium event photo & video coverage New Zealand wide,
+            capturing the biggest and best events in town. Working alongside
+            global and local brands alike, Sonna is the ‘go-to’ event
+            photographer and videographer in NZ and a preferred supplier to
+            government departments, charities, businesses, brands and
+            corporations.
+          </h2>
+        </div>
+        <div className="home-image-carousel-module"></div>
+        <div className="home-sticky-book-a-shoot-module">
+          <p>
+            We deliver premium event photo & video coverage New Zealand wide,
+            capturing the biggest and best events in town. Working alongside
+            global and local brands alike, Sonna is the ‘go-to’ event
+            photographer and videographer in NZ and a preferred supplier to
+            government departments, charities, businesses, brands and
+            corporations.We deliver premium event photo & video coverage New
+            Zealand wide, capturing the biggest and best events in town. Working
+            alongside global and local brands alike, Sonna is the ‘go-to’ event
+            photographer and videographer in NZ and a preferred supplier to
+            government departments, charities, businesses, brands and
+            corporations.
+          </p>
+          <button>Book A Shoot</button>
+        </div>
+        <div className="home-sticky-scroll-photography-module">
+          <h3>
+            We deliver premium event photo & video coverage New Zealand wide,
+            capturing the biggest and best events in town. Working alongside
+            global and local brands alike, Sonna is the ‘go-to’ event
+            photographer and videographer in NZ and a preferred supplier to
+            government departments, charities, businesses, brands and
+            corporations.
+          </h3>
+          <button>View All Photography</button>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
