40dcf4fe64e4bcd917a1c760720e75baaead7dee
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-04-26 15:43:17 +1000

    feat(BookingBlock): Remove unused imports and empty component definition

--- components/bookComponents/BookingBlock.tsx
+++ components/bookComponents/BookingBlock.tsx
@@ -1,11 +1,1 @@
-import { HOME_QUERYResult } from "@/sanity/types";
-import "../grid.css";
 
-type bookingBlockProps = Extract<
-  NonNullable<NonNullable<HOME_QUERYResult>["content"]>[number],
-  { _type: "bookingBlock" }
->;
-
-export function BookingBlock({}: bookingBlockProps) {
-  return <section className="grid"></section>;
-}
