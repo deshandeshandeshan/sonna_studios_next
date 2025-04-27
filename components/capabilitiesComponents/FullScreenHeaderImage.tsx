afedf98e1117817b36121ecf4b3f600f4e424dc5
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-04-27 12:00:29 +1000

    feat(FullScreenHeaderImage): Add styling and description support to FullScreenHeaderImage component

--- components/capabilitiesComponents/FullScreenHeaderImage.tsx
+++ components/capabilitiesComponents/FullScreenHeaderImage.tsx
@@ -1,24 +1,30 @@
 import { urlFor } from "@/sanity/lib/image";
 import { PAGE_QUERYResult } from "@/sanity/types";
 import Image from "next/image";
+import "./FullScreenHeaderImage.css";
 
 type fullScreenHeaderImageProps = Extract<
   NonNullable<NonNullable<PAGE_QUERYResult>["content"]>[number],
   { _type: "fullScreenHeaderImage" }
 >;
 
-export function FullScreenHeaderImage({ image }: fullScreenHeaderImageProps) {
+export function FullScreenHeaderImage({
+  image,
+  description,
+}: fullScreenHeaderImageProps) {
   return (
     <section>
-      <div>
+      <div className="header-image">
         {image ? (
           <Image
             src={urlFor(image).url()}
             width={1600}
             height={800}
             alt={image.caption || ""}
+            className="header-img caption-spacing"
           />
         ) : null}
+        <p className="type-heading mobile-padding">{description}</p>
       </div>
     </section>
   );
