a4b72e8f0930be6aa1493c3a6a7207488fdd32b9
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-05-15 20:42:35 +1000

    chore: update dependencies in package.json
    - upgraded next from 15.2.1 to ^15.3.2
    - added @next/third-parties dependency at ^15.3.2

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { HOME_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/sanity-utils";
 import { PageBuilder } from "@/components/PageBuilder";
 import { notFound } from "next/navigation";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const revalidate = 5;
 
@@ -12,5 +13,10 @@ export default async function Home() {
     notFound();
   }
 
-  return page?.content ? <PageBuilder content={page.content} /> : null;
+  return page?.content ? (
+    <>
+      <GoogleTagManager gtmId="GTM-KRGFGW5Z" />
+      <PageBuilder content={page.content} />
+    </>
+  ) : null;
 }
