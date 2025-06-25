c77a8f3d5be3c4c3510c67934f95d8439b12dc5e
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-06-25 11:09:09 +1000

    feat(Home): add Microsoft Clarity tracking script to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,19 @@ export default async function Home() {
         src="//js-ap1.hs-scripts.com/441814948.js"
         strategy="afterInteractive"
       />
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "ruhveaz6s0");
+    `,
+        }}
+      />
       <PageBuilder content={page.content} />
     </>
   ) : null;
